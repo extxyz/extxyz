3206b6c9adad95f94ea2ca8a523c040d9b111eb0
Jusong Yu <jusong.d.yu@gmail.com>
2026-04-08 20:57:54 +0200

    update the read_frame mem footprint bench

--- extxyz/examples/read_frames.rs
+++ extxyz/examples/read_frames.rs
@@ -5,7 +5,6 @@ use extxyz::read_frames;
 fn main() {
     let path = concat!(env!("CARGO_MANIFEST_DIR"), "/examples/32768_frames.xyz");
 
-    // read entire file into a String
     let file = fs::File::open(path).expect("Failed to read file");
 
     // wrap in Cursor for BufRead (Cursor<&[u8]> works, convert via as_bytes())
